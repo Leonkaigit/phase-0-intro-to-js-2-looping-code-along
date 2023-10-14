a67582e901e2f3e2165cc02ec40d8e5ee3e2426a
LeonGitonga <leon.gitonga@student.moringaschool.com>
2023-10-14 10:40:28 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+// Function to write "thank you" messages for each name and return them in an array
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+  // Example usage of writeCards function
+  const names = ["Alice", "Bob", "Charlie"];
+  const eventName = "birthday";
+  const messages = writeCards(names, eventName);
+  console.log(messages);
+  
+  // Function to count down from a positive integer to 0 using console.log
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  // Example usage of countDown function
+  countDown(10);
+  
